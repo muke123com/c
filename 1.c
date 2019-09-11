96d5237f4fc9dba8d61fe3ec11f870b1d0ff1415
muke123com <muke123com@163.com>
2019-09-11 10:35:47 +0800

    基础

--- 1.c
+++ 1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <float.h>
  
 int main()
 {
    printf("123\n");
    printf("int存储大小：%lu\n", sizeof(int));
    printf("float存储大小：%lu\n", sizeof(float));
+   printf("float 最小值： %E\n", FLT_MIN );
+   printf("float 最大值：%E\n", FLT_MAX);
+   printf("long存储大小：%lu\n", sizeof(long));
    return 0;
 }
