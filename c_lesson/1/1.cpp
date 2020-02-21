9596c265dc67830f18e8aed90ae456686cef4075
muke123com <muke123com@163.com>
2020-02-21 18:02:59 +0800

    w

---
+++ c_lesson/1/1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "1.h"
+
+// 全局函数的实现体
+void Fun2() { printf("1111"); }
+
+void Fun2();
+// // 类成员函数的实现体
+// void Box::Draw() {}
---
+++ c_lesson/1/1.h
@@ -0,0 +1,7 @@
+#ifndef ONE_H  // 防止 1.h 被重复引用
+
+#define ONE_H
+#include <math.h>
+void Fun1() {}   // 全局函数的实现体
+
+#endif
